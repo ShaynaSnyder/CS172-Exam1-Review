31dc3403c91dcbdd354099dca8a95329d0e3f553
ssnyder19 <ssnyder19@LAB-LIB-223-05>
2016-10-04 14:04:02 -0700

    minor changes

--- Exam1 Review/Exam1 Review/Dice.cpp
+++ Exam1 Review/Exam1 Review/Dice.cpp
@@ -8,12 +8,11 @@ Dice::Dice(int sides) //constructor sets Dice object using int sides
 {
 
 }
-				// The constructor also calls srand() to initialize 
-				// the random number generator.
-int Roll(int sides)// Returns the results of a roll (random value between 1 and sides)
+				// The constructor also calls srand() to initialize the random number generator.
+int Dice::Roll()// Returns the results of a roll (random value between 1 and sides)
 {
 	int rolls;
-	
+	rolls = rand() % 6 + 1;
 	return rolls;
 }
 int Dice::GetRolls() // Returns the number of times this dice was rolled since it was created.
