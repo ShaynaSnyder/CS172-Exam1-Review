7143290094e0376b7a51949e965c1947724bd64a
ssnyder19 <ssnyder19@LAB-LIB-223-05>
2016-10-05 19:06:18 -0700

    Solution

--- Exam1 Review/Exam1 Review/Dice.cpp
+++ Exam1 Review/Exam1 Review/Dice.cpp
@@ -1,6 +1,5 @@
 #pragma once
-#include <iomanip>
-#include <ctime>
+#include <iostream>
 #include <cstdlib>
 #include "Dice.h" //including Dice header file
 using namespace std;
@@ -8,22 +7,24 @@ using namespace std;
 
 Dice::Dice(int sides) //constructor sets Dice object using int sides
 {
-	srand(time(NULL));
+	//checks the number of sides is valid
+	if (sides <= 0)
+		sides = 6;
+	numSides = sides;
+	numRolls = 0;
 }
-				// The constructor also calls srand() to initialize the random number generator.
 int Dice::Roll()// Returns the results of a roll (random value between 1 and sides)
 {
-	int rolls;
-	rolls = rand() % sides + 1;
-	return rolls;
+	//Increments the number of times this dice was rolled
+	numRolls++;
+	//random number generator is seeded in main
+	return (rand() % numSides) + 1;
 }
 int Dice::GetRolls() // Returns the number of times this dice was rolled since it was created.
 {
-	int rolls;
-	rolls = Roll();
-	return rolls;
+	return numRolls;
 }
 int Dice::GetSides() // Returns the number of sides for this dice object.
 {
-	return sides;
+	return numSides;
 }
--- Exam1 Review/Exam1 Review/Dice.h
+++ Exam1 Review/Exam1 Review/Dice.h
@@ -1,17 +1,19 @@
 #pragma once
-#include <iomanip>
-#include <ctime>
-#include <cstdlib>
-#include "stdio.h"
+#include <stdio.h>
 
 //creates Dice class
 class Dice
 {
-private:	//declares int variable sides as private
-	int sides;
+private:	//declares int variables numSides and numRolls as private
+	int numSides;
+	int numRolls;
 public:		//declares functions as public
+	//creates a dice with the given number of sides
 	Dice(int sides);
+	//Rolls the dice and declares a random number
 	int Roll();
+	//Returns the number of times this dice was rolled
 	int GetRolls();
+	//Retuens the number of sides on this dice
 	int GetSides();
 };
